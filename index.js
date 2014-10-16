c510cf9c6170416b400b9aa4671c4a827ac86566
Lev Taydakov <lev.taydakov@gmail.com>
2014-10-16 15:25:47 -0700

    intendation changed

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var net = require('net');
 
-var server = net.createServer(function(c) { //'connection' listener
-  console.log('server connected');
-  console.log(c.remoteAddress);
-  console.log(c.remotePort);
-  c.on('end', function() {
-    console.log('server disconnected');
-  });
-  c.write('hello\r\n');
+var server = net.createServer(function(c) {
+	console.log('server connected');
+	console.log(c.remoteAddress);
+	console.log(c.remotePort);
+	c.on('end', function() {
+		console.log('server disconnected');
+	});
 
 	var client = net.connect(
 	{
@@ -17,13 +16,13 @@ var server = net.createServer(function(c) { //'connection' listener
 	  console.log('client connected');
 	});
 	client.on('end', function() {
-	  console.log('client disconnected');
+		console.log('client disconnected');
 	});
 
-  c.pipe(client);
-  client.pipe(c);
+	c.pipe(client);
+	client.pipe(c);
 });
 
-server.listen(80, function() { //'listening' listener
-  console.log('server bound');
+server.listen(80, function() {
+	console.log('server bound');
 });
